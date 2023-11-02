24fc48507778d407e949b8dc604063adf44d4fcc
Niiischal <nischal.khatiwada745@gmail.com>
2023-11-02 12:08:42 +0545

    added rules to the text fields in login and signup form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,16 +2,23 @@ import { Button, Form } from "antd";
 import Input from "antd/es/input/Input";
 import { Link } from "react-router-dom";
 
+const rules= [
+  {
+    required: true,
+    message: "required",
+  }
+]
+
 const Login = () => {
   return (
     <div className="h-screen flex justify-center items-center">
     <div className="form-container p-5 rounded-sm w-[350px] border-solid border border-[#2a68ff] ">
       <h1 className="text-[30px] my-2">Login</h1>
       <Form layout="vertical">
-        <Form.Item label="Email" name="email" className="font-semibold">
+        <Form.Item label="Email" name="email" className="font-semibold" rules={rules}>
           <Input type=" email" placeholder="Enter Your Email"></Input>
         </Form.Item>
-        <Form.Item label="Password" name="password" className="font-semibold">
+        <Form.Item label="Password" name="password" className="font-semibold" rules={rules}>
           <Input placeholder="Enter Your Password" type="password"></Input>
           <li className="mt-3">Forgot Password?</li>
         </Form.Item>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -2,19 +2,26 @@ import { Button, Form } from "antd";
 import Input from "antd/es/input/Input";
 import { Link } from "react-router-dom";
 
+const rules= [
+  {
+    required: true,
+    message: "required",
+  }
+]
+
 const Signup = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <div className="form-container p-5 rounded-sm w-[350px] border-solid border border-[#2a68ff]">
         <h1 className="text-[30px]  my-2">Create an Account</h1>
         <Form layout="vertical">
-          <Form.Item label="Full Name" name="name" className="font-semibold">
+          <Form.Item label="Full Name" name="name" className="font-semibold" rules={rules}>
             <Input placeholder="Enter Your Full Name"></Input>
           </Form.Item>
-          <Form.Item label="Email" name="email" className="font-semibold">
+          <Form.Item label="Email" name="email" className="font-semibold" rules={rules}>
             <Input type=" email" placeholder="Enter Your Email"></Input>
           </Form.Item>
-          <Form.Item label="Password" name="password" className="font-semibold">
+          <Form.Item label="Password" name="password" className="font-semibold" rules={rules}>
             <Input placeholder="Enter Your Password" type="password"></Input>
           </Form.Item>
           <Button type="primary" htmlType="" block>
