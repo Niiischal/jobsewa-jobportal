063a243797a3949c525cf16dc4a62cbebc60d1e4
Niiischal <nischal.khatiwada745@gmail.com>
2023-11-02 14:22:13 +0545

    connected mongodb to node server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const app = express();
 
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
 dbConfig();
 
-const port = process.env.PORT || 5000;
+const app = express();
+const port = 5000;
+
+//Middleware
+app.use(express.json())
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}`)
