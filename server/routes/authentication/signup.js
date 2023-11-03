d4e229009c36808440ae10c63f7f1037ddc73b5d
Niiischal <nischal.khatiwada745@gmail.com>
2023-11-03 19:47:53 +0545

    still signup endpoint problem

--- server/routes/authentication/signup.js
+++ server/routes/authentication/signup.js
@@ -15,7 +15,7 @@ router.post("/register", async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.password, salt)
     req.body.password = hashedPassword;
-    
+
     // create new user
     const newUser = new User(req.body);
     await newUser.save();
