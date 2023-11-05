80921ccbfb275a1467ffece749455f08206ccd8d
Niiischal <nischal.khatiwada745@gmail.com>
2023-11-05 10:30:12 +0545

    added role to schema on user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      default: "user"
+    },
   },
   {
     timestamps: true,
