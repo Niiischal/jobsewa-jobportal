8e0e11c0f513da9d350f1d6216c33b75fd2b0d63
Niiischal <nischal.khatiwada745@gmail.com>
2023-12-26 21:44:03 +0545

    created a new navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,17 +2,15 @@ import { Button } from "antd";
 
 const Navbar = () => {
   return (
-      <div className="navbar flex justify-between items-centeer pl-[2rem] pr-[2rem]">
+      <div className="navbar flex justify-between items-center pl-[2rem] pr-[2rem]">
         <div className="logo-div">
-          <h1 className="logo text-[25.4px] text-primary">
+          <h1 className="logo text-[27px] text-primary">
             JobSewa
           </h1>
         </div>
-        <div className="nav-items flex gap-6 pt-[1.6rem]">
-            <li className="nav-lists hover:text-primary text-[#848484]">Home</li>
-            <li className="nav-lists hover:text-primary text-[#848484]">About</li>
-            <li className="nav-lists hover:text-primary text-[#848484]">Employees/Post Job</li>
-            <li className="nav-lists hover:text-primary text-[#848484]">Sign In</li>
+        <div className="nav-items flex gap-9 items-center">
+            <li className=" hover:text-primary text-[#848484] text-[1.1rem] ">Login</li>
+            <li className=""><Button type="primary">Post A Job</Button></li>
         </div>
       </div>
   );
