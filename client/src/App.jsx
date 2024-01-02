0a7031c8cb8c4afe29feadac63adabf1f3a3c981
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-02 17:36:47 +0545

    created route for going to forgot password page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import ProtectedPage from "./components/ProtectedPage";
 import ProtectedHome from "./pages/JobSeeker/Home";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
               </ProtectedPage>
             }
           />
+          <Route path="/forgotPassword" element={<ForgotPassword/>} />
         </Routes>
       </Router>
     </div>
