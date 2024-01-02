c2248badcb16f8c41ac9d226723a83f11877e3e6
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-02 17:55:09 +0545

    formatting

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -1,35 +1,46 @@
 import { Button, Form, Input, message } from "antd";
-import React from 'react';
-import {Link} from "react-router-dom";
-import Navbar from '../components/Navbar';
+import React from "react";
+import { Link } from "react-router-dom";
+import Navbar from "../components/Navbar";
 
 function ForgotPassword() {
-  
-    const rules = [{ required: true, message: "This field is required" }];
+  const rules = [{ required: true, message: "This field is required" }];
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="h-screen flex justify-center items-center">
-    <div className="form-container p-5 rounded-sm w-[350px] border-solid border border-primary ">
-      <h1 className="text-[30px] my-2">Forgot Password?</h1>
-      <p className="font-medium text-base my-3">
+        <div className="form-container p-5 rounded-sm w-[350px] border-solid border border-primary ">
+          <h1 className="text-[30px] my-2">Forgot Password?</h1>
+          <p className="font-medium text-base my-3">
             Enter your email to reset your password
           </p>
-      <Form layout="vertical">
-        <Form.Item label="Email" name="email" className="font-semibold" rules={rules}>
-          <Input type=" email" placeholder="Enter Your Email"></Input>
-        </Form.Item>
-        <Button type="primary" className="bg-primary" htmlType="submit" block>
-          Get OTP
-        </Button>
-        <div className="mt-4 text-center text-base">
-          <Link to="/login" className="text-primary hover:text-black">Back to login</Link>
+          <Form layout="vertical">
+            <Form.Item
+              label="Email"
+              name="email"
+              className="font-semibold"
+              rules={rules}
+            >
+              <Input type=" email" placeholder="Enter Your Email"></Input>
+            </Form.Item>
+            <Button
+              type="primary"
+              className="bg-primary"
+              htmlType="submit"
+              block
+            >
+              Get OTP
+            </Button>
+            <div className="mt-4 text-center text-base">
+              <Link to="/login" className="text-primary hover:text-black">
+                Back to login
+              </Link>
+            </div>
+          </Form>
         </div>
-      </Form>
-    </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default ForgotPassword
+export default ForgotPassword;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,24 +4,24 @@ import { Link, useNavigate } from "react-router-dom";
 import { LoginUser } from "../apicalls/users";
 import Navbar from "../components/Navbar";
 
-const rules= [
+const rules = [
   {
     required: true,
     message: "required",
-  }
-]
+  },
+];
 
 const Login = () => {
   const navigate = useNavigate();
   const onFinish = async (values) => {
     try {
       const response = await LoginUser(values);
-      if (response.success){
+      if (response.success) {
         message.success(response.message);
         localStorage.setItem("token", response.data);
         window.location.href = "/jobseeker-home";
-      } else{
-        throw new Error(response.message)
+      } else {
+        throw new Error(response.message);
       }
     } catch (error) {
       message.error(error.message);
@@ -30,38 +30,53 @@ const Login = () => {
 
   useEffect(() => {
     if (localStorage.getItem("token")) {
-      navigate("/jobseeker-home")
+      navigate("/jobseeker-home");
     }
-  })
+  });
 
   return (
     <>
-    <Navbar label="Signup"/>
-    <div className="h-screen flex justify-center items-center">
-    <div className="form-container p-5 rounded-sm w-[350px] border-solid border border-primary ">
-      <h1 className="text-[30px] my-2">Login</h1>
-      <Form layout="vertical" onFinish={onFinish}>
-        <Form.Item label="Email" name="email" className="font-semibold" rules={rules}>
-          <Input type=" email" placeholder="Enter Your Email"></Input>
-        </Form.Item>
-        <Form.Item label="Password" name="password" className="font-semibold" rules={rules}>
-          <Input placeholder="Enter Your Password" type="password"></Input>
-        </Form.Item>
-        <Button type="primary" className="bg-primary" htmlType="" block>
-          Sign In
-        </Button>
-        <Link to="/forgotPassword" className="mt-5 text-black font-semibold">Forgot your Password?</Link>
-        <div className="mt-4 text-center text-base">
-        <span>
-            Not a Member?{" "}
-          </span>
-          <Link to="/register" className="text-primary hover:text-black">Create Account</Link>
+      <Navbar label="Signup" />
+      <div className="h-screen flex justify-center items-center">
+        <div className="form-container p-5 rounded-sm w-[350px] border-solid border border-primary ">
+          <h1 className="text-[30px] my-2">Login</h1>
+          <Form layout="vertical" onFinish={onFinish}>
+            <Form.Item
+              label="Email"
+              name="email"
+              className="font-semibold"
+              rules={rules}
+            >
+              <Input type=" email" placeholder="Enter Your Email"></Input>
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              className="font-semibold"
+              rules={rules}
+            >
+              <Input placeholder="Enter Your Password" type="password"></Input>
+            </Form.Item>
+            <Button type="primary" className="bg-primary" htmlType="" block>
+              Sign In
+            </Button>
+            <Link
+              to="/forgotPassword"
+              className="mt-5 text-black font-semibold"
+            >
+              Forgot your Password?
+            </Link>
+            <div className="mt-4 text-center text-base">
+              <span>Not a Member? </span>
+              <Link to="/register" className="text-primary hover:text-black">
+                Create Account
+              </Link>
+            </div>
+          </Form>
         </div>
-      </Form>
-    </div>
-  </div>
-  </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
