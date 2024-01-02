2d546236dec67290a2f18565e647bda53e410cd2
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-02 17:13:46 +0545

    added forgot password option

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -47,11 +47,11 @@ const Login = () => {
         </Form.Item>
         <Form.Item label="Password" name="password" className="font-semibold" rules={rules}>
           <Input placeholder="Enter Your Password" type="password"></Input>
-          {/* <li className="mt-2 font-semibold hover:text-primary">Forgot Password?</li> */}
         </Form.Item>
         <Button type="primary" className="bg-primary" htmlType="" block>
           Sign In
         </Button>
+        <Link to="forgotPassword" className="mt-5 text-black font-semibold">Forgot your Password?</Link>
         <div className="mt-4 text-center text-base">
         <span>
             Not a Member?{" "}
