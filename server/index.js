2b3927081bb8a46fa0429ef821b51f63f637638b
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-07 21:43:58 +0545

    CORS issue solved

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require('cors');
+
 const app = express();
 app.use(express.json())
+app.use(cors());
 
 require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
