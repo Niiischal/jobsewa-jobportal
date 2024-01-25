934d9cdb3bdb40b9e88a025250d1a06179524e05
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-25 14:43:08 +0545

    stored the slice into the store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { loadersSlice } from "./loadersSlice";
+import { usersSlice } from "./usersSlice";
 
 const store = configureStore({
     reducer: {
         loaders: loadersSlice.reducer,
+        users: usersSlice.reducer,
         },
 })
 
