fb0fc647392c49c88773a3ac6d2907ba08867b90
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-25 14:28:01 +0545

    the loader component with its position was added into app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,22 @@ import ProtectedHome from "./pages/JobSeeker/Home";
 import Login from "./pages/Login";
 import OTPVerification from "./pages/OTP";
 import Signup from "./pages/Signup";
+import Loader from "./components/Loader";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { loading } = useSelector((state) => state.loaders);
+
   return (
     <div>
+      {loading && <Loader />}
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
-          <Route path="/forgotPassword" element={<ForgotPassword/>} />
-          <Route path="/verifyOTP" element= {<OTPVerification/>} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/verifyOTP" element={<OTPVerification />} />
           <Route
             path="/jobseeker-home"
             element={
