150323a0948c93623a02f481e47406473475cb6e
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-26 21:17:07 +0545

    slight changes into the resume upload component

--- client/src/components/UploadResume.jsx
+++ client/src/components/UploadResume.jsx
@@ -1,36 +1,32 @@
-import { InboxOutlined } from '@ant-design/icons';
-import { message, Upload } from 'antd';
-import React from 'react';
-const { Dragger } = Upload;
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Upload } from "antd";
+import React from "react";
+import { SlCloudUpload } from "react-icons/sl";
+const UploadResume = () => {
+
+  const handleUpload = async () => {
+  }
+
+  return (
+    <div className="flex justify-center items-center mt-5 flex-col border border-solid border-primary p-6">
+      <div className="flex items-center flex-col">
+        <SlCloudUpload size={150} color="#2a68ff" />
+        <h1>Upload Your Resume.</h1>
+        <p>Upload your resume to ensure employers can easily find you</p>
+      </div>
+      <Upload listType="picture" beforeUpload={() => false}>
+        <Button type="primary" icon={<UploadOutlined/>}>Select or Drag drop your resume</Button>
+      </Upload>
+      <Button
+        onChange={handleUpload}
+        style={{
+          marginTop: 16,
+          width: 200,
+        }}
+      >
+        Upload
+      </Button>
+    </div>
+  );
 };
-const App = () => (
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <InboxOutlined/>
-    </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-    <p className="ant-upload-hint">
-      Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-      banned files.
-    </p>
-  </Dragger>
-);
-export default App;
+export default UploadResume;
