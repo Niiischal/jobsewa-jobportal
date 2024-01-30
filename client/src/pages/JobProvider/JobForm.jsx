1372ee21803d2eadbf777eede54a930a7a2e502d
Niiischal <nischal.khatiwada745@gmail.com>
2024-01-30 15:48:55 +0545

    created an array to add the options and mapped it using a function for each inputs

--- client/src/pages/JobProvider/JobForm.jsx
+++ client/src/pages/JobProvider/JobForm.jsx
@@ -1,15 +1,74 @@
 import { Form, Input, Modal, Select, Tabs } from "antd";
 import React from "react";
 
+const categoryOptions = [
+  "Accounting / Finance",
+  "Architecture / Interior Design",
+  "Banking / Insurance / Financial Services",
+  "Commercial / Logistics / Supply Chain",
+  "Construction / Engineering / Architects",
+  "Creative / Content / Graphics / Video Editing",
+  "Hospitality / Tourism",
+  "IT / Telecommunication",
+];
+
+const durationOptions = ["14 Days", "30 Days"];
+
+const levelOptions = [
+  "Entry Level",
+  "Junior Level",
+  "Senior Level",
+  "Mid Level",
+];
+
+const educationOptions = [
+  "below SLC/SEE",
+  "SLC/SEE",
+  "Intermediate",
+  "Bachelors",
+  "Post-Graduate-Diploma",
+  "Masters",
+  "MPhil",
+  "PHD",
+];
+
+const experienceOptions = [
+  "Fresher",
+  "6 months",
+  "1 year",
+  "2 year",
+  "3 year",
+  "4 year",
+  "5 years",
+  "More than 5 years",
+];
+
+const salaryPeriodOptions = ["Weekly", "Monthly", "Yearly"];
+
+const salaryAmountOptions = [
+  "5K-10K",
+  "10K",
+  "10K-15K",
+  "15K-20K",
+  "20K-25K",
+  "25K-30K",
+  "30K-40K",
+  "40K-50K",
+  "50K-75K",
+  "75K-100K",
+  "100K-Above",
+  "Negotiable",
+];
+
 const skillsOptions = [
-    "Accounting",
-    "Analytics & Data Science",
-    "Counseling",
-    "Data Visualization",
-    "Digital Marketing",
-    "Web Design",
-    "Web Developer"
-]
+  "Accounting",
+  "Analytics & Data Science",
+  "Counseling",
+  "Data Visualization",
+  "Digital Marketing",
+  "Web Design",
+  "Web Developer",
+];
 
 function JobForm(showJobForm, setShowJobForm) {
   return (
@@ -25,264 +84,157 @@ function JobForm(showJobForm, setShowJobForm) {
         <Tabs defaultActiveKey="1">
           <Tabs.TabPane tab="Company Details" key="1">
             <Form layout="vertical">
-              <Form.Item  rules={[{required: true}]}   label="Company Name" name="Companyname">
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Company Name"
+                name="Companyname"
+              >
                 <Input type="text" required></Input>
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Company's Email" name="Companyemail">
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Company's Email"
+                name="Companyemail"
+              >
                 <Input type="text" required></Input>
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Location" name="Companylocation">
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Location"
+                name="Companylocation"
+              >
                 <Input type="text" required></Input>
               </Form.Item>
             </Form>
           </Tabs.TabPane>
           <Tabs.TabPane tab="Job Details" key="2">
             <Form layout="vertical">
-              <Form.Item  rules={[{required: true}]}   label="No of openings" name="openings">
+              <Form.Item
+                rules={[{ required: true }]}
+                label="No of openings"
+                name="openings"
+              >
                 <Input type="number" required></Input>
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Category" name="category" >
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Category"
+                name="category"
+              >
                 <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "Accounting / Finance",
-                      value: "Accounting / Finance",
-                    },
-                    {
-                      label: "Architecture / Interior Design",
-                      value: "Architecture / Interior Design",
-                    },
-                    {
-                      label: "Banking / Insurance / Financial Services",
-                      value: "Banking / Insurance / Financial Services",
-                    },
-                    {
-                      label: "Commercial / Logistics / Supply Chain",
-                      value: "Commercial / Logistics / Supply Chain",
-                    },
-                    {
-                      label: "Construction / Engineering / Architects",
-                      value: "Construction / Engineering / Architects",
-                    },
-                    {
-                      label: "Creative / Content / Graphics / Video Editing",
-                      value: "Creative / Content / Graphics / Video Editing",
-                    },
-                    {
-                      label: "Hospitality / Tourism",
-                      value: "Hospitality / Tourism",
-                    },
-                    {
-                      label: "IT / Telecommunication",
-                      value: "IT / Telecommunication",
-                    },
-                  ]}
+                  options={categoryOptions.map((category) => ({
+                    label: category,
+                    value: category,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Duration" name="duration">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Duration"
+                name="duration"
+              >
+                <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "14 Days",
-                      value: "14 Days",
-                    },
-                    {
-                      label: "30 Days",
-                      value: "30 Days",
-                    },
-                  ]}
+                  options={durationOptions.map((duration) => ({
+                    label: duration,
+                    value: duration,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Job Level" name="level">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Job Level"
+                name="level"
+              >
+                <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "Entry Level",
-                      value: "Entry Level",
-                    },
-                    {
-                      label: "Junior Level",
-                      value: "Junior Level",
-                    },
-                    {
-                      label: "Senior Level",
-                      value: "Senior Level",
-                    },
-                    {
-                      label: "Mid Level",
-                      value: "Mid Level",
-                    },
-                  ]}
+                  options={levelOptions.map((level) => ({
+                    label: level,
+                    value: level,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Education required" name="education">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Education required"
+                name="education"
+              >
+                <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "below SLC/SEE",
-                      value: "below SLC/SEE",
-                    },
-                    {
-                      label: "SLC/SEE",
-                      value: "SLC/SEE",
-                    },
-                    {
-                      label: "Intermediate",
-                      value: "Intermediate",
-                    },
-                    {
-                      label: "Bachelors",
-                      value: "Bachelors",
-                    },
-                    {
-                      label: "Post-Graduate-Diploma",
-                      value: "Post-Graduate-Diploma",
-                    },
-                    {
-                      label: "Masters",
-                      value: "Masters",
-                    },
-                    {
-                      label: "MPhil",
-                      value: "MPhil",
-                    },
-                    {
-                      label: "PHD",
-                      value: "PHD",
-                    },
-                  ]}
+                  options={educationOptions.map((education) => ({
+                    label: education,
+                    value: education,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Experience required" name="experience">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Experience required"
+                name="experience"
+              >
+                <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "Fresher",
-                      value: "Fresher",
-                    },
-                    {
-                      label: "6 months",
-                      value: "6 months",
-                    },
-                    {
-                      label: "1 year",
-                      value: "1 year",
-                    },
-                    {
-                      label: "2 year",
-                      value: "2 year",
-                    },
-                    {
-                      label: "3 year",
-                      value: "3 year",
-                    },
-                    {
-                      label: "4 year",
-                      value: "4 year",
-                    },
-                    {
-                      label: "5 years",
-                      value: "5 years",
-                    },
-                    {
-                      label: "More than 5 years",
-                      value: "More than 5 years",
-                    },
-                  ]}
+                  options={experienceOptions.map((experience) => ({
+                    label: experience,
+                    value: experience,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Salary Period" name="salaryperiod">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Salary Period"
+                name="salaryperiod"
+              >
+                <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "Weekly",
-                      value: "Weekly",
-                    },
-                    {
-                      label: "Monthly",
-                      value: "Monthly",
-                    },
-                    {
-                      label: "Yearly",
-                      value: "Yearly",
-                    }
-                  ]}
+                  options={salaryPeriodOptions.map((period) => ({
+                    label: period,
+                    value: period,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Salary Amount" name="salaryamount">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Salary Amount"
+                name="salaryamount"
+              >
+                <Select
                   defaultValue="--select--"
-                  options={[
-                    {
-                      label: "5K-10K",
-                      value: "5K-10K",
-                    },
-                    {
-                      label: "10K",
-                      value: "10K",
-                    },
-                    {
-                      label: "10K-15K",
-                      value: "10K-15K",
-                    },
-                    {
-                      label: "15K-20K",
-                      value: "15K-20K",
-                    },
-                    {
-                      label: "20K-25K",
-                      value: "20K-25K",
-                    },
-                    {
-                      label: "25K-30K",
-                      value: "25K-30K",
-                    },
-                    {
-                      label: "30K-40K",
-                      value: "30K-40K",
-                    },
-                    {
-                      label: "40K-50K",
-                      value: "40K-50K",
-                    },
-                    {
-                      label: "50K-75K",
-                      value: "50K-75K",
-                    },
-                    {
-                      label: "75K-100K",
-                      value: "75K-100K",
-                    },
-                    {
-                      label: "100K-Above",
-                      value: "100K-Above",
-                    },
-                    {
-                      label: "Negotiable",
-                      value: "Negotiable",
-                    },
-                  ]}
+                  options={salaryAmountOptions.map((amount) => ({
+                    label: amount,
+                    value: amount,
+                  }))}
                 />
               </Form.Item>
-              <Form.Item  rules={[{required: true}]}   label="Skills" name="skills">
-              <Select
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Skills"
+                name="skills"
+              >
+                <Select
                   placeholder="choose the skills"
                   mode="multiple"
-                  options={skillsOptions.map(skill => ({ label: skill, value:skill }))}
+                  options={skillsOptions.map((skill) => ({
+                    label: skill,
+                    value: skill,
+                  }))}
                 />
               </Form.Item>
-                  <Form.Item  rules={[{required: true}]}   label="Job Description"  name="description">
-                  <Input.TextArea rows={4}/>
-                  </Form.Item>
-                  <Form.Item  rules={[{required: true}]}   label="Job Specification"  name="specification">
-                  <Input.TextArea rows={4}/>
-                  </Form.Item>
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Job Description"
+                name="description"
+              >
+                <Input.TextArea rows={4} />
+              </Form.Item>
+              <Form.Item
+                rules={[{ required: true }]}
+                label="Job Specification"
+                name="specification"
+              >
+                <Input.TextArea rows={4} />
+              </Form.Item>
             </Form>
           </Tabs.TabPane>
         </Tabs>
