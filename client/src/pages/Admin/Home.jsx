3ba8f3b1a3d7ce48bbbfb48fe553e427da5db735
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-08 17:20:55 +0545

    added the tab rendering as per the admin role

--- client/src/pages/Admin/Home.jsx
+++ client/src/pages/Admin/Home.jsx
@@ -1,9 +1,25 @@
 import { Tabs } from "antd";
 import React from "react";
+import { useSelector } from "react-redux";
+import JobDetails from "./JobDetails";
+import UserDetails from "./UserDetails";
 
 function Home() {
+  const { user } = useSelector((state) => state.users);
   return (
     <div>
+              {user.role === "admin" && (
+          <div className="navigation pl-[2rem] pr-[2rem]">
+            <Tabs defaultActiveKey="1">
+              <Tabs.TabPane tab="Users" key="1">
+                <UserDetails/>
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Jobs" key="2">
+                <JobDetails/>
+              </Tabs.TabPane>
+            </Tabs>
+          </div>
+        )}
     </div>
   );
 }
