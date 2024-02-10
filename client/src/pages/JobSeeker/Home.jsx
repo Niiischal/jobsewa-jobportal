07a812a0695f1c6fb711750995db5221644e1446
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-10 13:33:12 +0545

    placing the tabs for job seeker in correct order

--- client/src/pages/JobSeeker/Home.jsx
+++ client/src/pages/JobSeeker/Home.jsx
@@ -1,29 +1,29 @@
-import { Tabs } from 'antd';
-import React from 'react';
+import { Tabs } from "antd";
+import React from "react";
 import { useSelector } from "react-redux";
-import Resume from './Resume';
-import Jobs from './Jobs';
+import Resume from "./Resume";
+import Jobs from "./Jobs";
 
 function Home() {
   const { user } = useSelector((state) => state.users);
   return (
     <div>
       {user.role === "jobSeeker" && (
-          <div>
-            <Tabs defaultActiveKey="1">
-              <Tabs.TabPane tab="Upload Resume" key="1">
-                <div className="flex justify-center items-center">
-                  <Resume/>
-                </div>
-              </Tabs.TabPane>
-              <Tabs.TabPane tab={<span>Job Seeker Tab 2</span>} key="2">
-                <Jobs/>
-              </Tabs.TabPane>
-            </Tabs>
-          </div>
-        )}
+        <div>
+          <Tabs defaultActiveKey="1">
+            <Tabs.TabPane tab="Availabe Jobs" key="1">
+              <Jobs />
+            </Tabs.TabPane>
+            <Tabs.TabPane tab="Resume" key="2">
+              <div className="flex justify-center items-center">
+                <Resume />
+              </div>
+            </Tabs.TabPane>
+          </Tabs>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
