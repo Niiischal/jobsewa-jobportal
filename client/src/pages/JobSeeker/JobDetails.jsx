79b74a5f6033176bed35e46f6be71de7e989398a
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-11 14:41:10 +0545

    changed the job deatils page to starting for now

--- client/src/pages/JobSeeker/JobDetails.jsx
+++ client/src/pages/JobSeeker/JobDetails.jsx
@@ -1,18 +1,11 @@
 import React from 'react'
 
-function JobDetails(props) {
-  const { selectedJob } = props.location.state;
+function JobDetails() {
   return (
-          <div className="w-full p-4 bg-white">
-      <div>
-        <h1>{`Content for ${selectedJob.category}`}</h1>
-        <p>{`Company: ${selectedJob.companyname}`}</p>
-        <p>{`Location: ${selectedJob.companylocation}`}</p>
-        {/* Render additional details or components related to the selected job */}
-      </div>
+    <div>
+      job details
     </div>
   )
 }
 
-
-export default JobDetails;
+export default JobDetails
