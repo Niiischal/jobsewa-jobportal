71bad978962fc6e314796140a26ed5d56ca14e2c
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-11 15:17:40 +0545

    change made into the api name

--- server/routes/jobRoute.js
+++ server/routes/jobRoute.js
@@ -47,7 +47,7 @@ router.post("/get-jobs", async (req, res) => {
   }
 });
 
-router.get("/get-product-by-id/:id", async (req, res) => {
+router.get("/get-job-by-id/:id", async (req, res) => {
   try {
     const job = await Job.findById(req.params.id).populate("jobProvider");
     res.send({
