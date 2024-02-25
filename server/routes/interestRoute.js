165a2070fc7821f0f94429e401d82a0474416fcb
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-25 22:13:41 +0545

    retrieving the jobSeeker from req.body in the route

--- server/routes/interestRoute.js
+++ server/routes/interestRoute.js
@@ -1,21 +1,41 @@
-const router = require("express").Router()
-const Interest = require("../models/interestModel")
+const router = require("express").Router();
+const Interest = require("../models/interestModel");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/post-interests", authMiddleware, async (req, res) => {
-    try {
-      const newInterest = new Interest(req.body);
-      await newInterest.save();
-      res.send({
-        success: true,
-        message: "New interest added successfully!",
-      });
-    } catch (error) {
-      res.send({
-        success: false,
-        message: error.message,
-      });
+  try {
+    const newInterest = new Interest(req.body);
+    await newInterest.save();
+    res.send({
+      success: true,
+      message: "New interest added successfully!",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.post("/get-interest", async (req, res) => {
+  try {
+    const {jobSeeker} = req.body
+    let filters = {};
+    if (jobSeeker) {
+      filters.jobSeeker = jobSeeker;
     }
-  });
+    const interests = await Interest.find(filters).populate("jobSeeker");
+    res.send({
+      success: true,
+      data: interests,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
