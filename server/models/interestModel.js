4077b303b57166deb067c10497ecf4079d1b15df
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-25 13:23:56 +0545

    created schema for interest

---
+++ server/models/interestModel.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const interestSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        education: {
+            type: Number,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            required: true,
+        },
+        experience: {
+            type: String,
+            required: true,
+        },
+        skills: {
+            type: Array,
+            required: true,
+        },
+        jobSeeker: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "users",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Interest = mongoose.model("interest", interestSchema);
+
+module.exports = Interest;
