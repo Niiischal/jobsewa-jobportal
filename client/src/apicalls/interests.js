dde9a2377f461f1cdc63f365c40b094cac036dd6
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-25 13:32:06 +0545

    created api call for post-interests

---
+++ client/src/apicalls/interests.js
@@ -0,0 +1,10 @@
+import { axiosInstance } from "./axiosInstance";
+
+export const PostInterest = async (payload) =>{
+    try {
+        const response = await axiosInstance.post("/api/interests/post-interests")
+        return response.data;
+    } catch (error) {
+        return { success: false, message: error.message };
+    }
+}
