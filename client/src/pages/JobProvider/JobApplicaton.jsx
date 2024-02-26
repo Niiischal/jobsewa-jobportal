bcb929944f68dfd85c99da32d2d934030747683c
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-26 14:32:02 +0545

    made some changes

--- client/src/pages/JobProvider/JobApplicaton.jsx
+++ client/src/pages/JobProvider/JobApplicaton.jsx
@@ -1,5 +1,4 @@
 import { Card, message } from "antd";
-import { formatDistanceToNow } from "date-fns";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
@@ -15,7 +14,7 @@ function JobApplication() {
     try {
       dispatch(SetLoader(true));
       const response = await Applicants(id);
-      console.log(response.data)
+      console.log(response.data);
       dispatch(SetLoader(false));
       if (response.success) {
         SetApplicants(response.data);
@@ -30,30 +29,27 @@ function JobApplication() {
     getData();
   }, []);
 
-  const formatDate = (dateString) => {
-    return formatDistanceToNow(new Date(dateString), { addSuffix: true });
-  };
-
   return (
     <div className="flex justify-between gap-4">
       {applicants.map((user) => (
-          <Card
-            key={user._id} // Use the user's ID as the key
-            className="cursor-pointer shadow-lg hover:shadow-xl transition duration-300"
-            title={user.name} // Display the user's name as the card title
-          >
-            <div className="flex items-center gap-3 mb-2 text-[13px] text-gray-600">
-              <p>Email: {user.email}</p> {/* Display the user's email */}
-              <p>Role: {user.role}</p> {/* Display the user's role */}
-            </div>
-            <div>
-              <p>Applied Jobs:</p>
-              {user.appliedJobs.map((jobId) => (
-                <p key={jobId}>{jobId}</p> // Display each applied job ID
-              ))}
-            </div>
-          </Card>
-        ))}
+        <Card
+          key={user._id} // Use the user's ID as the key
+          className="cursor-pointer shadow-lg hover:shadow-xl transition duration-300"
+          title={user.name}
+        >
+          <div className="flex items-center gap-3 mb-2 text-[13px] text-gray-600">
+            <p>Email: {user.email}</p>
+            <p>Role: {user.role}</p>
+            <p>Pdf: {user.pdf}</p>
+          </div>
+          <div>
+            <p>Applied Jobs:</p>
+            {user.appliedJobs.map((jobId) => (
+              <p key={jobId}>{jobId}</p>
+            ))}
+          </div>
+        </Card>
+      ))}
     </div>
   );
 }
