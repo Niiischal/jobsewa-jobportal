aee642c29bce6ec29a6469d2abe6c78d017c6272
Niiischal <nischal.khatiwada745@gmail.com>
2024-02-26 14:08:43 +0545

    added api call for editing or updating the interest

--- client/src/apicalls/interests.js
+++ client/src/apicalls/interests.js
@@ -1,19 +1,37 @@
 import { axiosInstance } from "./axiosInstance";
 
-export const PostInterest = async (payload) =>{
-    try {
-        const response = await axiosInstance.post("/api/interests/post-interests", payload)
-        return response.data;
-    } catch (error) {
-        return { success: false, message: error.message };
-    }
-}
+export const PostInterest = async (payload) => {
+  try {
+    const response = await axiosInstance.post(
+      "/api/interests/post-interests",
+      payload
+    );
+    return response.data;
+  } catch (error) {
+    return { success: false, message: error.message };
+  }
+};
 
 export const GetInterests = async (filters) => {
-    try {
-      const response = await axiosInstance.post("/api/interests/get-interest", filters);
-      return response.data;
-    } catch (error) {
-      return { success: false, message: error.message };
-    }
-  };
+  try {
+    const response = await axiosInstance.post(
+      "/api/interests/get-interest",
+      filters
+    );
+    return response.data;
+  } catch (error) {
+    return { success: false, message: error.message };
+  }
+};
+
+export const EditInterest = async (id, payload) => {
+  try {
+    const response = await axiosInstance.put(
+      `/api/interests/edit-interests/${id}`,
+      payload
+    );
+    return response.data;
+  } catch (error) {
+    return { success: false, message: error.message };
+  }
+};
