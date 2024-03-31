8be3cc3e29b1375b138c9a45dcce2b0b7ed83e32
Niiischal <nischal.khatiwada745@gmail.com>
2024-03-31 17:34:05 +0545

    added modal while clicking on the posted job

--- client/src/pages/JobProvider/Jobs.jsx
+++ client/src/pages/JobProvider/Jobs.jsx
@@ -1,4 +1,4 @@
-import { Button, Card, message, Pagination, Popconfirm, Tag } from "antd";
+import { Button, Card, message, Modal, Pagination, Popconfirm, Table, Tag } from "antd";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,6 +13,7 @@ function Jobs() {
   const [currentPage, setCurrentPage] = useState(1);
   const [jobsPerPage] = useState(4); // Number of jobs per page
   const { user } = useSelector((state) => state.users);
+  const [showJobModal, setShowJobModal] = useState(false);
   const dispatch = useDispatch();
 
   const getData = async () => {
@@ -85,6 +86,54 @@ function Jobs() {
     setCurrentPage(page);
   };
 
+  const JobDetailsTable = ({ selectedJob }) => {
+    const columns = [
+      {
+        title: "Title",
+        dataIndex: "title",
+        render: (text) => <span style={{ fontWeight: "600" }}>{text}</span>,
+      },
+      {
+        title: "Value",
+        dataIndex: "value",
+      },
+    ];
+
+    const data = [
+      {
+        key: "openings",
+        title: "Job Openings",
+        value: selectedJob.openings,
+      },
+      {
+        key: "salaryperiod",
+        title: "Salary Period",
+        value: selectedJob.salaryperiod,
+      },
+      {
+        key: "salaryamount",
+        title: "Salary Amount",
+        value: selectedJob.salaryamount,
+      },
+      {
+        key: "skills",
+        title: "Job Skills",
+        value: selectedJob.skills,
+      },
+    ];
+
+    return (
+      <Table
+        columns={columns}
+        dataSource={data}
+        pagination={false}
+        bordered={false}
+        showHeader={false}
+      />
+    );
+  };
+
+
   return (
     <div>
       <div className="flex justify-end">
@@ -104,6 +153,10 @@ function Jobs() {
             className="bg-[#fafafa] cursor-pointer shadow-lg hover:shadow-xl transition duration-300"
             key={job._id}
             title={job.category}
+            onClick={() => {
+              setSelectedJob(job); 
+              setShowJobModal(true); 
+            }}
           >
             <p>
               Added On: {moment(job.createdAt).format("DD-MM-YYYY hh:mm A")}
@@ -155,6 +208,59 @@ function Jobs() {
           getData={getData}
         />
       )}
+      {currentJobs.map((job) => (
+        <Modal
+          open={showJobModal}
+          onCancel={() => setShowJobModal(false)}
+          centered
+          width={"90%"}
+          footer={null}
+          key={job._id}
+        >
+          <div className="font-proxima mt-5">
+            <div className="pl-[10px] pr-[10px] pt[0] rounded-lg border border-gray-200 shadow-lg">
+              <div className="flex gap-4">
+                <div className="flex justify-between flex-col">
+                  <div>
+                    <h2>{job.category}</h2>
+                  </div>
+                  <div className="flex items-center gap-2 text-[12px] text-gray-600">
+                    <span>{job.companyname}</span>
+                    <span>{job.companylocation}</span>
+                    <span>{job.companyemail}</span>
+                  </div>
+                  <div className="flex items-center gap-1 my-5 border-b dark:border-gray-900">
+                    <Tag color="blue">{job.level}</Tag>
+                    <Tag color="green">{job.type}</Tag>
+                    <Tag color="red">{job.education}</Tag>
+                    <Tag color="orange">{job.experience}</Tag>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="pl-[10px] pr-[10px]">
+              <div className="details flex flex-col gap-3">
+                <div className="descprition">
+                  <h3>Description</h3>
+                  <span className="text-[14px] text-gray-500">
+                    {job.description}
+                  </span>
+                </div>
+                <div className="specification">
+                  <h3>Specification</h3>
+                  <span className="text-[14px] text-gray-500">
+                    {job.specification}
+                  </span>
+                </div>
+                <div className="facts">
+                  <h3>Numbers & Facts</h3>
+                  <JobDetailsTable selectedJob={selectedJob} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      ))}
     </div>
   );
 }
