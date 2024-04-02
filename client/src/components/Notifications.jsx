bf7b3d214bc6eddc252565a956cfd0eb87027aa1
Niiischal <nischal.khatiwada745@gmail.com>
2024-04-02 16:34:16 +0545

    navigation while clicking on the notification

--- client/src/components/Notifications.jsx
+++ client/src/components/Notifications.jsx
@@ -1,5 +1,6 @@
 import { Divider, Modal } from "antd";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Notifications({
   notifications = [],
@@ -7,6 +8,7 @@ function Notifications({
   showNotification,
   setShowNotifications,
 }) {
+  const navigate = useNavigate();
   return (
     <Modal
       title="Notifications"
@@ -18,7 +20,14 @@ function Notifications({
     >
       <div className="flex flex-col gap-2">
         {notifications.map((notification) => (
-          <div className="flex flex-col border border-solid p-2 border-gray-300 rounded">
+          <div
+            className="flex flex-col border border-solid p-2 border-gray-300 rounded cursor-pointer"
+            key={notification._id}
+            onClick={() => {
+              navigate(notification.onClick);
+              setShowNotifications(false);
+            }}
+          >
             <h1 className="text-gray-700">{notification.title}</h1>
             <span className="text-gray-500">{notification.message}</span>
           </div>
