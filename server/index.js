994472e194ab4dd911105187e4febeffb325eb09
Niiischal <nischal.khatiwada745@gmail.com>
2024-04-10 00:09:00 +0545

    added routes of chat and messages in the index

--- server/index.js
+++ server/index.js
@@ -17,11 +17,15 @@ const jobRoute = require("./routes/jobRoute");
 const interestRoute = require("./routes/interestRoute");
 const resumeRoute = require("./routes/resumeRoute");
 const notificationRoute = require("./routes/notificationRoute");
+const chatRoute = require("./routes/chatRoute");
+const messageRoute = require("./routes/messageRoute");
 
 app.use("/api/users", userRoute);
 app.use("/api/jobs", jobRoute);
 app.use("/api/interests", interestRoute);
 app.use("/api/resumes", resumeRoute);
 app.use("/api/notifications", notificationRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
